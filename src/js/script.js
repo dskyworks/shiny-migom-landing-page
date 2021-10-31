764e458ba44657e062d275e171febc93567bfab5
Vladislav Dribinsky <dskyworks@yandex.ru>
2021-10-31 22:13:18 +0300

    Добавляет блокировку скролла при открытом меню

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,20 @@
 
 // Menu open/close & aria attributes
 
+const html = document.querySelector('.html');
 const menuButton = document.querySelector('.menu__button');
 const header = document.querySelector('.header');
 const menu = document.querySelector('.menu');
+
 menuButton.addEventListener('click', () => {
   let expanded = menuButton.getAttribute('aria-expanded') === 'true';
+
   menuButton.setAttribute('aria-expanded', !expanded);
   expanded
     ? menuButton.setAttribute('aria-label', 'Открыть меню')
     : menuButton.setAttribute('aria-label', 'Закрыть меню');
+
+  html.classList.toggle('html--lock');
   header.classList.toggle('header--open-menu');
   menu.classList.toggle('menu--open');
   menuButton.classList.toggle('menu__button--open');
@@ -19,6 +24,7 @@ menuButton.addEventListener('click', () => {
 // Hero background animation
 
 const heroBackground = document.querySelector('.hero');
+
 const observer = new IntersectionObserver((e) => {
   console.log(e[0].isIntersecting);
   e[0].isIntersecting
@@ -31,5 +37,6 @@ const observer = new IntersectionObserver((e) => {
         heroBackground.style.backgroundPosition = 'center bottom';
       });
 });
+
 observer.observe(heroBackground);
 
